bffea6d48b7aae5289058771dec45c825c6b875e
cboard <168048361+cboard9@users.noreply.github.com>
2025-04-28 11:14:33 -0400

    Transaction Fetching - Created endpoints for fetching all transactions, transactions based on buyer, and transactions based on seller. - Created transaction dto, userarchive dto, and mappers

--- src/main/java/com/cboard/marketplace/marketplace_backend/dao/TransactionDao.java
+++ src/main/java/com/cboard/marketplace/marketplace_backend/dao/TransactionDao.java
@@ -3,7 +3,10 @@ package com.cboard.marketplace.marketplace_backend.dao;
 import com.cboard.marketplace.marketplace_backend.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionDao extends JpaRepository<Transaction, Integer>
 {
-
+    List<Transaction> findByBuyer_UserId(int buyerId);
+    List<Transaction> findBySeller_UserId(int sellerId);
 }
--- src/main/java/com/cboard/marketplace/marketplace_backend/service/TransactionService.java
+++ src/main/java/com/cboard/marketplace/marketplace_backend/service/TransactionService.java
@@ -2,9 +2,14 @@ package com.cboard.marketplace.marketplace_backend.service;
 
 
 import com.cboard.marketplace.marketplace_backend.dao.TransactionDao;
+import com.cboard.marketplace.marketplace_backend.dao.UserArchiveDao;
+import com.cboard.marketplace.marketplace_backend.dao.UserDao;
+import com.cboard.marketplace.marketplace_backend.model.DtoMapping.TransactionMapper;
+import com.cboard.marketplace.marketplace_backend.model.DtoMapping.UserMapper;
 import com.cboard.marketplace.marketplace_backend.model.Transaction;
 import com.cboard.marketplace.marketplace_backend.model.User;
 import com.cboard.marketplace.marketplace_backend.model.UserItems;
+import com.cboard.marketplace.marketplace_common.TransactionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +22,66 @@ import java.util.List;
 public class TransactionService
 {
     @Autowired
-    TransactionDao dao;
+    TransactionDao transactionDao;
+    @Autowired
+    UserArchiveDao userArchiveDao;
+    @Autowired
+    TransactionMapper transactionMapper;
 
 
-    public ResponseEntity<List<Transaction>> getAllTransactions()
+
+    public ResponseEntity<List<TransactionDto>> getAllTransactions()
     {
+        List<Transaction> transactions = transactionDao.findAll();
+        List<TransactionDto> dtos = new ArrayList<>();
         try
         {
-            return new ResponseEntity<>(dao.findAll(), HttpStatus.OK);
+            for(Transaction transaction : transactions)
+                dtos.add(transactionMapper.transactionToDto(transaction));
+
+            return new ResponseEntity<>(dtos, HttpStatus.OK);
         }
         catch(Exception e)
         {
             e.printStackTrace();
+            return new ResponseEntity<>(dtos, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
+    public ResponseEntity<List<TransactionDto>> getAllTransactionsByBuyer(int buyerId)
+    {
+        List<Transaction> transactions = transactionDao.findByBuyer_UserId(buyerId);
+        List<TransactionDto> dtos = new ArrayList<>();
+        try
+        {
+            for(Transaction transaction : transactions)
+                dtos.add(transactionMapper.transactionToDto(transaction));
+
+            return new ResponseEntity<>(dtos, HttpStatus.OK);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity<>(dtos, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public ResponseEntity<List<TransactionDto>> getAllTransactionsBySeller(int sellerId)
+    {
+        List<Transaction> transactions = transactionDao.findBySeller_UserId(sellerId);
+        List<TransactionDto> dtos = new ArrayList<>();
+        try
+        {
+            for(Transaction transaction : transactions)
+                dtos.add(transactionMapper.transactionToDto(transaction));
+
+            return new ResponseEntity<>(dtos, HttpStatus.OK);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity<>(dtos, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
